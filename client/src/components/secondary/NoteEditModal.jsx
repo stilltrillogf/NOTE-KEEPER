c556c172bfeae25f4e69a6f1b7d341b942e36373
stilltrillogf <2004kowalewicz@gmail.com>
2023-02-28 20:27:49 +0100

    Add blurred overlay when NoteEditModal is active;

--- client/src/components/secondary/NoteEditModal.jsx
+++ client/src/components/secondary/NoteEditModal.jsx
@@ -71,40 +71,40 @@ export const NoteEditModal = ({ note, isEditingState }) => {
   }, [handleClickOutside]);
 
   return (
-    <div ref={modalRef} className={styles.modal}>
-      <div
-        className={styles.customTitleTextbox}
-        suppressContentEditableWarning="true"
-        contentEditable={true}
-        onInput={(e) => {
-          console.log(updatedNote);
-
-          setUpdatedNote({ ...updatedNote, title: e.target.textContent });
-        }}
-      >
-        {note.title}
-      </div>
-      <div
-        className={styles.customTextTextbox}
-        suppressContentEditableWarning="true"
-        contentEditable={true}
-        ref={textInputRef}
-        onInput={(e) => {
-          console.log(updatedNote);
-          setUpdatedNote({ ...updatedNote, text: e.target.textContent });
-        }}
-      >
-        {note.text}
-      </div>
-      <div className={styles.footer}>
-        <div className={styles.options}>
-          <BsTrash className={styles.option} onClick={handleDeleteNote} />
+    <>
+      <div className={styles.modalOverlay}></div>
+      <div ref={modalRef} className={styles.modal}>
+        <div
+          className={styles.customTitleTextbox}
+          suppressContentEditableWarning="true"
+          contentEditable={true}
+          onInput={(e) => {
+            setUpdatedNote({ ...updatedNote, title: e.target.textContent });
+          }}
+        >
+          {note.title}
         </div>
-        <div onClick={handleFinishEditing} className={styles.closeBtn}>
-          Close
+        <div
+          className={styles.customTextTextbox}
+          suppressContentEditableWarning="true"
+          contentEditable={true}
+          ref={textInputRef}
+          onInput={(e) => {
+            setUpdatedNote({ ...updatedNote, text: e.target.textContent });
+          }}
+        >
+          {note.text}
+        </div>
+        <div className={styles.footer}>
+          <div className={styles.options}>
+            <BsTrash className={styles.option} onClick={handleDeleteNote} />
+          </div>
+          <div onClick={handleFinishEditing} className={styles.closeBtn}>
+            Close
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
