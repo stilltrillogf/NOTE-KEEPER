70cbfcadc25e75d1bb923110e823ee0d4e5479a1
stilltrillogf <2004kowalewicz@gmail.com>
2023-03-01 18:04:30 +0100

    Add overlay and footer logic

--- client/src/components/secondary/Note.jsx
+++ client/src/components/secondary/Note.jsx
@@ -1,16 +1,28 @@
 import React, { useEffect, useRef, useState } from "react";
 import { BsPin } from "react-icons/bs";
+import { IoCheckmarkCircleOutline } from "react-icons/io5";
 
 import styles from "../../styles/Note.module.css";
 import { NoteEditModal } from "./NoteEditModal";
 
 export const Note = ({ note }) => {
   const [isEditing, setIsEditing] = useState(false);
+  const [noteIsHovered, setNoteIsHovered] = useState(true);
   const noteRef = useRef(null);
+
+  const handleClickNote = () => {
+    // TODO: If part of the overlay is clicked - return
+    setIsEditing(true);
+  };
 
   const handleHoverNote = (e) => {
     if (noteRef && noteRef.current.contains(e.target)) {
-      // console.log("hovering the note");
+      console.log("hovering the note");
+
+      // TODO: Make it so it doesn't re-fire when hovered over item that is INSIDE the note
+      // Intended behavior :
+      // -> once the cursor has entered the note, noteIsHovered state changes to true (overlay becomes visible)
+      // -> once the cursor has left the note, noteIsHovered state changes to false (overlay disappears)
     }
   };
 
@@ -25,20 +37,34 @@ export const Note = ({ note }) => {
   return (
     <>
       <div
-        onClick={() => {
-          setIsEditing(true);
-        }}
+        onClick={handleClickNote}
         className={styles.note}
         key={note._id}
         ref={noteRef}
       >
         <div className={styles.noteTitle}>{note.title}</div>
-
         <div className={styles.noteText}>{note.text}</div>
+        {noteIsHovered && <NoteOverlay />}
+        <div
+          className={`${styles.overlayFooter} ${
+            noteIsHovered ? "" : styles.hidden
+          }`}
+        >
+          FOOTER
+        </div>
       </div>
       {isEditing && (
         <NoteEditModal isEditingState={[isEditing, setIsEditing]} note={note} />
       )}
+    </>
+  );
+};
+
+const NoteOverlay = () => {
+  return (
+    <>
+      <IoCheckmarkCircleOutline className={styles.overlaySelect} size={25} />
+      <BsPin className={styles.overlayPin} size={25} />
     </>
   );
 };
